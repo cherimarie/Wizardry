84a8c2f60da5d27cc14c308b6d514dcbc0285c4d
cherimarie <cherimarie@gmail.com>
2012-11-25 18:20:35 -0800

    so many php errors

--- index.php
+++ index.php
@@ -14,30 +14,32 @@
 
 include 'include/config-mini.php';
 $title = 'Wizard Name Generator'; //Optional unique variable becomes title tag
-#$sql = "Select * from produce_List"; 
-$first = "Select first_name from Wizard_data where fisrt_name like 'A%'"
+$sql = "Select * from Wizard_data"; 
+$first = "Select first_name from Wizard_data where first_name like 'A%'"
 
 #--- end config area
 
 include 'touching_design/touching_top.php'; #header must appear before any HTML is printed by PHP
-echo '<h3>Wizard Name Generator</h3>';
+echo '<h3>Wizard Name Generator1</h3>';
 $myConn = @mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) or die(myerror(__FILE__,__LINE__,mysqli_connect_error()));
 $result = mysqli_query($myConn,$sql) or die(myerror(__FILE__,__LINE__,mysqli_error($myConn)));
-/*if (mysqli_num_rows($result) > 0)//at least one record!
+if (mysqli_num_rows($result) > 0)//at least one record!
 {show results
 	while ($row = mysqli_fetch_assoc($result))
     {
 	  echo "<p>";
-	   echo "ProduceID: <b>" . $row['ProduceID'] . "</b><br />";
-	   echo "Produce Name: <b>" . $row['ProduceName'] . "</b><br />";
-	   echo "Description: <b>" . $row['Description'] . "</b><br />";
-	   echo '<a href ="produce_view.php?id=' . $row['ProduceID'] . '">' . $row['ProduceName'] . '</a><br />';
+	   echo "ProduceID: <b>" . $row['NameID'] . "</b><br />";
+	   echo "Produce Name: <b>" . $row['first _name'] . "</b><br />";
+	   echo "Description: <b>" . $row['middle_name'] . "</b><br />";
+	   echo "Description: <b>" . $row['last_name'] . "</b><br />";
+	   echo "Description: <b>" . $row['superpower'] . "</b><br />";
+	  // echo '<a href ="produce_view.php?id=' . $row['NameID'] . '">' . $row['ProduceName'] . '</a><br />';
 	   echo "</p>";
    }
 }else{//no records
 	print '<div align="center">No produce? I\'d better get back to the field!</div>';*/
-echo "<p>$first</p>"; 
-	echo "<p>is this thing on?</p>";
+//echo "<p>$first</p>"; 
+
 }
 
 @mysqli_free_result($result); # releases web server resources
